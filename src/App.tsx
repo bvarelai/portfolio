e04edf32eb4937f29cbc93d6463f1795df5565a6
bvarelai <brais.sieiro@udc.es>
2026-07-22 12:06:23 +0200

    Mas arreglos

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import Education from './education'
 import Projects from './proyects'
 import Contact from './contact'
@@ -30,27 +30,27 @@ function App() {
             <SunIcon onClick={toggleTheme} className='icon'/> :<MoonIcon onClick={toggleTheme} className='icon'/>}  
           </div>  
           <div className="menu-web">
-            <Link to="/portfolio">Home</Link>
-            <Link to="/portfolio/education">Education</Link>
-            <Link to="/portfolio/projects">Projects</Link>
-            <Link to="/portfolio/contact">Contact</Link>      
+            <Link to="/">Home</Link>
+            <Link to="/education">Education</Link>
+            <Link to="/projects">Projects</Link>
+            <Link to="/contact">Contact</Link>      
           </div>
           <div className='menu-web-mobile'>
             <button className="menu-btn" onClick={() => setOpen(!open)}>☰</button>
             <nav className={`menu ${open ? "open" : ""}`} onClick={() => setOpen(false)}>
-              <Link to="/portfolio">Home</Link>
-              <Link to="/portfolio/education">Education</Link>
-              <Link to="/portfolio/projects">Projects</Link>
-              <Link to="/portfolio/contact">Contact</Link>      
+              <Link to="/">Home</Link>
+              <Link to="/education">Education</Link>
+              <Link to="/projects">Projects</Link>
+              <Link to="/contact">Contact</Link>      
             </nav>
           </div>
         </header>
         <main className='main'>
           <Routes>
-            <Route path="/portfolio" element={<Home />} />
-            <Route path="/portfolio/education" element={<Education />} />
-            <Route path="/portfolio/projects" element={<Projects />} />
-            <Route path="/portfolio/contact" element={<Contact/>} />
+            <Route path="/" element={<Home />} />
+            <Route path="/education" element={<Education />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/contact" element={<Contact/>} />
           </Routes>
         </main>
         <footer className='web-footer'>
